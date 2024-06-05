dda48a3e36a2dda254a0c3cb31432c0a2db40b26
daccordodan <daccordodan@gmail.com>
2024-06-05 12:54:26 +0200

    Creted files

---
+++ src/main/java/org/unipd/myAdapter/HCollection.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.unipd.myAdapter;
+
+/**
+ *
+ * @author dacco
+ */
+public class HCollection {
+
+}
