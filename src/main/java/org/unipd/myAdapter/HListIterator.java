c0ed19fa6f39c41748511a78d2d3790ecf4da4d6
daccordodan <daccordodan@gmail.com>
2024-06-05 18:13:44 +0200

    fixed

--- src/main/java/org/unipd/myAdapter/HListIterator.java
+++ src/main/java/org/unipd/myAdapter/HListIterator.java
@@ -4,20 +4,14 @@ package org.unipd.myAdapter;
 public interface HListIterator extends HIterator{
     void add(Object o);
         //Inserts the specified element into the list (optional operation).
-    boolean hasNext();
-        //Returns true if this list iterator has more elements when traversing the list in the forward direction.
     boolean hasPrevious();
         //Returns true if this list iterator has more elements when traversing the list in the reverse direction.
-    Object next();
-        //Returns the next element in the list.
     int nextIndex();
         //Returns the index of the element that would be returned by a subsequent call to next.
     Object previous();
         //Returns the previous element in the list.
     int previousIndex();
         //Returns the index of the element that would be returned by a subsequent call to previous.
-    void remove();
-        //Removes from the list the last element that was returned by next or previous (optional operation).
     void set(Object o);
         //Replaces the last element returned by next or previous with the specified element (optional operation).
 }
